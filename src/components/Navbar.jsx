9202b26ef7d9145984974c3f1da3a9f662cf5834
nishikanta7819 <nishikantabehera7819.com>
2025-05-24 20:56:56 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         </div>
         <div className="account">
           <p>my account</p>
-          <img src="" alt="" />
+          {/* <img src="" alt="" /> */}
         </div>
       </div>
       <div className="navbar-bottom">
